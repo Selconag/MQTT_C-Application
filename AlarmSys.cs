d53cfc657b76a03f8ca71804411449ee202a48ea
Selconag <ogrengeek@gmail.com>
2023-12-21 17:35:22 +0300

    Initial Commit on Project

---
+++ AlarmSys.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+// Define a custom EventArgs class to pass additional information with the event
+public class AlarmEventArgs : EventArgs
+{
+    public string Message { get; set; }
+
+    public AlarmEventArgs(string message)
+    {
+        Message = message;
+    }
+}
+
+// Define a class for the alarm system
+public class AlarmSystem
+{
+    // Define the delegate (if using non-generic pattern).
+    public delegate void AlarmEventHandler(object sender, AlarmEventArgs e);
+
+    // Define the event using the generic pattern.
+    public event AlarmEventHandler AlarmTriggered;
+
+    // Method to simulate the alarm going off
+    public void TriggerAlarm()
+    {
+        Console.WriteLine("Alarm is triggered!");
+        OnAlarmTriggered("Danger! Intruder detected.");
+    }
+    
+    // Method to raise the AlarmTriggered event
+    protected virtual void OnAlarmTriggered(string message)
+    {
+        // Raise the event only if there are subscribers
+        AlarmTriggered?.Invoke(this, new AlarmEventArgs(message));
+    }
+}
+
+// Define a class that will respond to the alarm event
+public class SecurityMonitor
+{
+    // Event handler method
+    public void HandleAlarm(object sender, AlarmEventArgs e)
+    {
+        Console.WriteLine($"Security Monitor received alarm: {e.Message}");
+    }
+}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        // Create instances of the alarm system and security monitor
+//        AlarmSystem alarmSystem = new AlarmSystem();
+//        SecurityMonitor securityMonitor = new SecurityMonitor();
+
+//        // Subscribe the security monitor to the alarm event
+//        alarmSystem.AlarmTriggered += securityMonitor.HandleAlarm;
+
+//        // Trigger the alarm to see the event in action
+//        alarmSystem.TriggerAlarm();
+
+//        Console.ReadLine(); // Pause to see the output
+//    }
+//}
---
+++ Publisher.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Server;
+
+namespace MQTTPublisher;
+
+public class Publisher
+{
+    static async Task Main(string[] args)
+    {
+        var mqttFactory = new MqttFactory();
+        IMqttClient client = mqttFactory.CreateMqttClient();
+
+        //Build it first
+        var options = mqttFactory.CreateClientOptionsBuilder()
+            .WithClientId(Guid.NewGuid().ToString())
+            .WithTcpServer("test.mosquitto.org" , 1883)
+            .WithCleanSession()
+            .Build();
+
+
+        client.UseConnectedHandler(e =>
+        {
+            Console.WriteLine("Connected to the Broker Succesfully");
+        });
+
+        client.UseDisconnectedHandler(e =>
+        {
+            Console.WriteLine("Disconnected to the Broker Succesfully");
+        });
+
+        await client.ConnectAsync(options);
+
+        Console.WriteLine($"Houston we have a connection\n");
+        Console.WriteLine($"Houston press a key or something!\n");
+        Console.ReadLine();
+
+        await PublishMessageAsync(client);
+
+        await client.DisconnectAsync();
+
+    }
+
+    private static async Task PublishMessageAsync(IMqttClient client)
+    {
+        string messagePayload = "What!";
+        var message = new MqttApplicationMessageBuilder()
+            .WithTopic("Houston")
+            .WithPayload(messagePayload)
+            .WithAtLeastOnceQoS()
+            .Build();
+        if (client.IsConnected)
+        {
+            await client.PublishAsync(message);
+            Console.WriteLine($"Published Message: {message}");
+
+        }
+    }
+
+    public void ConnectionMessagePublish(MqttClientConnectionStatus status)
+    {
+        Debug.WriteLine($"Houston we have a {status}");
+    }
+}
+
